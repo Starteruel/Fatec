11792b76e1d7884cca272733b6075ddcfbcd9a96
starteruel <starteruel@gmail.com>
2024-09-17 11:23:45 -0300

    Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsTableComponent } from './products-table/products-table.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo : '/products', pathMatch: 'full'},
+  {path: 'products', component: ProductsTableComponent},
+  {path: 'product/:id', component: ProductItemComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsTableComponent } from './products-table/products-table.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsTableComponent
+    ProductsTableComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
