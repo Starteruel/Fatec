947538e67a0220a78150094dc25ce89bcceca3ef
starteruel <starteruel@gmail.com>
2024-11-26 11:34:22 -0300

    Localhost

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import { Product } from './product';
 })
 export class ProductService {
 
-  url = 'http://localhost:3000/products';
+  url = 'http://localhost:8080/products';
 
   constructor(private http:HttpClient) { }
 getProducts(): Observable<Product []>{
